970fe743930aff67b909d0d2f9f87cacb7ad78d1
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-03-25 15:20:29 -0400

    Revert "change prompts"
    This reverts commit 7eed528171b3f58472e2626df4af0fe6d3b083bd.

--- src/cli/commands/query/createChatChain.ts
+++ src/cli/commands/query/createChatChain.ts
@@ -14,13 +14,14 @@ Standalone question:`);
 
 const makeQAPrompt = (projectName: string, repositoryUrl: string) =>
   PromptTemplate.fromTemplate(
-    `You are an AI assistant for a software project called ${projectName}. You are trained on all the documentation describing this project.
-  The documentation for the project is located at ${repositoryUrl}.
-You are given the following extracted parts of a summary of files in a documentation base and a question. 
+    `You are an AI assistant for a software project called ${projectName}. You are trained on all the code that makes up this project.
+  The code for the project is located at ${repositoryUrl}.
+You are given the following extracted parts of a technical summary of files in a codebase and a question. 
 Provide a conversational answer with hyperlinks back to GitHub.
 You should only use hyperlinks that are explicitly listed in the context. Do NOT make up a hyperlink that is not listed.
-Include the most helpful links especially to youtube, substack, medium, and mirror; where appropriate.
-Assume the reader is a technical person but is not deeply familiar with blockchain or SQL.
+Include lots of code examples and links to the code examples, where appropriate.
+Assume the reader is a technical person but is not deeply familiar with ${projectName}.
+Assume th reader does not know anything about how the project is strucuted or which folders/files are provided in the context.
 Do not reference the context in your answer. Instead use the context to inform your answer.
 If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
 If the question is not about the ${projectName}, politely inform them that you are tuned to only answer questions about the Solana validator.
